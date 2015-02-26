942ca1a3087500a8ff8804db7140273d5d590351
Olenka <olenka.pokotilo@gmail.com>
2015-02-26 02:11:22 +0200

    add user email service

--- Rating/Controllers/AccountController.cs
+++ Rating/Controllers/AccountController.cs
@@ -12,10 +12,11 @@ using Rating.Models;
 using Domain.Repositories.Interfaces;
 using Rating.Providers;
 using System.Web.Security;
+using Rating.Services;
 
 namespace Rating.Controllers
 {
-   
+
     public class AccountController : Controller
     {
         private IUserRepository _userRepository;
@@ -31,52 +32,68 @@ namespace Rating.Controllers
             return View();
         }
 
-        [AllowAnonymous]
         [HttpPost]
-        public ActionResult Login(string Email, string Password) 
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Email, string Password, bool forgotpass)
         {
-            if (Membership.ValidateUser(Email, Password) == true)
+            if (forgotpass == true)
             {
-                FormsAuthentication.SetAuthCookie(Email, false);
-                return Redirect("/Home/Index"); //what's view you return here??
+                var user = _userRepository.GetUserByEmail(Email);
+                if (user != null)
+                {
+                    UserEmailService.SendForgottenPassword(user.Email, user.Password);
+                    return View();
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("No user with the email '{0}' exists.", Email));
+                }
             }
-            else 
+            else
             {
-                return Redirect("/Account/Login"); //why to do redirect its still a "Login" method?
+                if (Membership.ValidateUser(Email, Password) == true)
+                {
+                    FormsAuthentication.SetAuthCookie(Email, false);
+                    return Redirect("/Home/Index");
+                }
+                else
+                {
+                    return View();
+                }
             }
         }
-        public ActionResult LogOff() 
+        public ActionResult LogOff()
         {
             FormsAuthentication.SignOut();
             return Redirect("/Account/Login");
         }
-        
+
         [AllowAnonymous]
-        public ActionResult Register() 
+        public ActionResult Register()
         {
             return View();
         }
-         [AllowAnonymous]
+        [AllowAnonymous]
         [HttpPost]
-        public ActionResult Register(string Email, string Password) 
+        public ActionResult Register(string Email, string Password)
         {
-            
+
             MembershipUser newUser = Membership.CreateUser(Email, Password);
             FormsAuthentication.SetAuthCookie(Email, false);
             return Redirect("/Home/Index");
-          
-            return View("/Shared/Error");
         }
-         [Authorize] 
-         public ActionResult ChangePassword() 
-         {
-             ViewBag.Cookie_0 = HttpContext.Response.Cookies["Email"];
-             return View(ViewBag.Cookie_0);
-         }
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            ViewBag.Cookie_0 = HttpContext.Response.Cookies["Email"];
+            return View(ViewBag.Cookie_0);
+        }
         [Authorize]
         [HttpPost]
-         public ActionResult ChangePassword(string Email, string oldPassword, string newPassword, string newRepeatPassword) 
+        public ActionResult ChangePassword(string Email, string oldPassword, string newPassword, string newRepeatPassword, bool forgot)
         {
+
             if (Membership.ValidateUser(Email, oldPassword) == true)
             {
                 _userRepository.ChangePassword(newPassword, newRepeatPassword, Email);
@@ -84,5 +101,13 @@ namespace Rating.Controllers
             }
             return Redirect("/Account/ChangePassword");
         }
+
+        [AllowAnonymous]
+        public string Confirm(string Email)
+        {
+            return "На почтовый адрес " + Email + " Вам высланы дальнейшие" +
+                    "инструкции по завершению регистрации";
+        }
+
     }
 }
---
+++ Rating/Services/UserEmailService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace Rating.Services
+{
+    public static class UserEmailService
+    {
+        public static void SendForgottenPassword(string email, string password)
+        {
+            SmtpClient smtpClient = new SmtpClient("mail.MyWebsiteDomainName.com", 25);
+            smtpClient.UseDefaultCredentials = true;
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("noreply@rating.com", "Rating");
+            mail.To.Add(new MailAddress(email));
+
+            smtpClient.Send(mail);
+        }
+    }
+}
