7dd7cffe1b31b25041d1c6e5712b922f55407ff5
Olenka <olenka.pokotilo@gmail.com>
2015-03-09 23:01:17 +0200

    test commit from VS

--- Rating/Controllers/ProjectController.cs
+++ Rating/Controllers/ProjectController.cs
@@ -39,5 +39,6 @@ namespace Rating.Controllers
             return PartialView(_projectRepository.GetAllProjectByUserId(id).Select(p => ProjectModel.FromDomainModel(p)));
 
         }
+        public void Foo() { }
     }
 }
