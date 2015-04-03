825b2946416b8fbbc21ae1fefc80f5737094995d
Olenka <olenka.pokotilo@gmail.com>
2015-04-03 14:11:35 +0200

    add in domain entities rating, action, projectUser

---
+++ Domain/Entities/Action.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Action
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public int ActioTypeId { get; set; }
+        public int ProjectUserId { get; set; }
+        public int? RatingId { get; set; }
+    }
+}
---
+++ Domain/Entities/Rating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int RatingTypeId { get; set; }
+        public int? ProjectUsesrId { get; set; }
+    }
+}
