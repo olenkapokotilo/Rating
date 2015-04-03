0cd83cf8265ccba72b568b5b33b5fa8281e3a252
Olenka <olenka.pokotilo@gmail.com>
2015-04-03 13:57:33 +0200

    add repository in dataAccess for action, rating, projectUser

---
+++ DataAccess/Repositories/RatingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Repositories.Interfaces;
+
+namespace DataAccess.Repositories
+{
+    class RatingRepository : IRatingRepository
+    {
+    }
+}
