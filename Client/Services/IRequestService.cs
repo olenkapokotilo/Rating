b27263d1951931bc90b2796d5c98bf3105e4512f
ypokotylo <yevgeniy.pokotylo@infopulse.com>
2015-05-02 16:30:00 +0300

    services add folder with IRequestSwrvice and JsonRequestService

---
+++ Client/Services/IRequestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+        public interface IRequestService<T> where T: class
+        {
+            void Post(T data);
+            string Get();
+            void Put(T data);
+            void Delete(int id);
+        }
+}
