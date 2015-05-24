15dab7c7e14472618b28f52df62503505adfed10
Olenka <olenka.pokotilo@gmail.com>
2015-05-24 14:43:03 +0200

    add event name constatants

--- Client/Services/EventService.cs
+++ Client/Services/EventService.cs
@@ -5,20 +5,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Client.Services;
+using System.Configuration;
 
 
 namespace Client.Services
 {
     public class EventService
     {
-       // string[] listRatingTypes = new string[]{"Active", ""};
-        Dictionary<string, string[]> actions = new Dictionary<string, string[]>();
-        //actions.Add("Active", new string[]{"Comment","Like"});//???
-        public void Comment(int userId, string path) 
+        public void Comment(string userName, string path) 
         {
-           // var request = new JsonRequestService(path);
-            //request.Post("{actionType:'comment', raitntgType:'Active'}");
-            
+            var service = new JsonRequestService<ApiAction>(path);
+            service.Post(new ApiAction() { 
+                projectUserName  = userName,
+                projectId = RatingProjectId,
+                actionTypeName = EventConstants.ActiveRatingType.ActionTypeNames.Comment,
+                ratingTypeName = EventConstants.ActiveRatingType.Name
+            });
+        }
+
+        private static int RatingProjectId
+        {
+            get { return int.Parse(ConfigurationManager.AppSettings["RatingProjectId"]); }
         }
     }
+
+    public static class EventConstants
+    {
+        public static class ActiveRatingType
+        {
+            public const string Name = "Active";
+
+            public static class ActionTypeNames
+            {
+                public const string Comment = "Comment";
+                public const string Like = "Like";
+            }
+        }
+
+        public static class CuriousRatingType
+        {
+            public const string Name = "Curious";
+
+            public static class ActionTypeNames
+            {
+                public const string Comment = "Question";
+                public const string Comment = "OpenPage";
+            }
+        }
+    }
+
 }
