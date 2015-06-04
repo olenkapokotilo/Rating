e1eabf742136092c753a6904ff445958bcc6bb8a
Olenka <olenka.pokotilo@gmail.com>
2015-06-04 13:44:04 +0200

    add ratingListModel in client.services anh api.models

---
+++ Client/Services/RatingListModel.cs
@@ -0,0 +1,15 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client.Services
+{
+    public class RatingListModel
+    {
+        public string ExternalUserId { get; set; }
+        public int Scores { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ RatingAPI/Models/RatingListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RatingAPI.Models
+{
+    public class RatingListModel
+    {
+        public string ExternalUserId { get; set; }
+        public int Scores { get; set; }
+    }
+}
