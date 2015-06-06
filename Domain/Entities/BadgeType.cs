ec2385e66a52c0cf649f38dce339ce479d961d6d
Olenka <olenka.pokotilo@gmail.com>
2015-06-06 21:38:33 +0200

    add models in domain file and badgetype

---
+++ Domain/Entities/BadgeType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class BadgeType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Scores { get; set; }
+        public int RatingTypeId { get; set; }
+        public int FileId { get; set; }
+
+        public virtual RatingType RatingType { get; set; }
+        public virtual File File { get; set; }
+    }
+}
---
+++ Domain/Entities/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    class File
+    {
+        //public File()
+        //{
+        //    this.BadgeType = new HashSet<BadgeType>();
+        //}
+
+        public int Id { get; set; }
+        public byte[] Image { get; set; }
+
+        public virtual ICollection<BadgeType> BadgeType { get; set; }
+    }
+}
