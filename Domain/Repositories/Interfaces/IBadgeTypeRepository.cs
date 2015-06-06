ced6c41ebb7d9dd29096e564d7702c948b686dc1
Olenka <olenka.pokotilo@gmail.com>
2015-06-06 21:45:35 +0200

    add irepositiries for badgetype and file in domain

---
+++ Domain/Repositories/Interfaces/IBadgeTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories.Interfaces
+{
+    interface IBadgeTypeRepository
+    {
+        Domain.Entities.BadgeType GetBadgeType(int id);
+        IEnumerable<Domain.Entities.BadgeType> GetAllBadgeTypeByRatingType(int id);
+        void Create(Domain.Entities.BadgeType badgeType);
+        void Delete(int id);
+    }
+}
