06665db36a0ff848e9e676c66d1f93f1ae7a8ee7
Olenka <olenka.pokotilo@gmail.com>
2015-06-06 22:14:46 +0200

    add models in rating(mvc) filmodel and badgetypemodel

---
+++ Rating/Models/BadgeTypeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rating.Models
+{
+    public class BadgeTypeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Scores { get; set; }
+        public int RatingTypeId { get; set; }
+        public int FileId { get; set; }
+        //public virtual RatingType RatingType { get; set; }
+        public virtual FileModel File { get; set; }
+    }
+}
---
+++ Rating/Models/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rating.Models
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
